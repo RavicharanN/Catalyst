1b52d515d64a3cf055d13ec71d632b5ca390d2ff
RavicharanN <ravicharan.vsp@gmail.com>
2017-10-29 01:35:33 +0530

    Add skeleton

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
-from flask import Flask,render_template,url_for,redirect
+from flask import Flask,render_template,url_for,redirect,request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/server')
-def server():
+# @app.route('/processSend',methods = ['POST','GET'])
+# def processSend():
+#     if request.method == "GET":
+#         path = request.args.get('path')
+#         return path
+#     else:
+#         path = request.form
 
+@app.route('/send',methods = ['POST','GET'])
+def send():
+    if request.method == "GET":
+        return render_template('send.html')
+    else:
+        path = request.form['path']
+        print(path)
+        return path
 
+@app.route('/receive')
+def receive():
+    return render_template('receive.html')
 
-@app.route('/client')
-def client():
-
-
-if __name__ == __main__:
+if __name__ == "__main__":
     app.run(debug=True)
