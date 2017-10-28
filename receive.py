ab6bca1a928239b8b7f7de44780c323d1eb4b064
AdityaVallabh <adityavallabhganti@gmail.com>
2017-10-29 02:09:01 +0530

    Flask skeleton 1

---
+++ receive.py
@@ -0,0 +1,21 @@
+import socket
+
+def receive(ip, portNo):
+    s = socket.socket()
+    port = int(portNo)
+    s.connect((ip, port))
+    s.send(str.encode("Connected"))
+    filename = s.recv(1024).decode("utf-8")
+    s.send(str.encode("lel"))
+    with open("R_"+filename, 'wb') as f:
+        print('receiving file...')
+        while True:
+            data = s.recv(1024)
+            if not data:
+                break
+            f.write(data)
+
+    f.close()
+    print('Successfully get the file')
+    s.close()
+    print('connection closed')
---
+++ send.py
@@ -0,0 +1,37 @@
+import socket
+import os
+import ntpath
+
+
+def send(path, port):
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    host = s.getsockname()[0]
+    s.close()
+    s = socket.socket()
+    s.bind((host, int(port)))
+    s.listen(5)
+
+    print('Server listening....')
+    print(host, int(port))
+
+    conn, addr = s.accept()
+    print ('Got connection from' + str(addr))
+    data = conn.recv(1024)
+
+    filepath = str(path)
+    filename = ntpath.basename(filepath)
+    f = open(filepath,'rb')
+    l = f.read(1024)
+    print ("Sending file " + filename)
+
+    conn.send(str.encode(filename))
+    conn.recv(1024)
+    while (l):
+       conn.send(l)
+       l = f.read(1024)
+    f.close()
+
+    print('Done sending')
+    conn.close()
